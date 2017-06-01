acefea326107bb8fa68576d28f4a58313a6e5ee3
Guillermo <guillermomm1230@gmail.com>
2017-06-01 19:51:11 +0200

    Falla nomes una cosa del pagament que no trobo

--- PracticaECSDI/Messages/PaymentRequestMessage.py
+++ PracticaECSDI/Messages/PaymentRequestMessage.py
@@ -13,24 +13,20 @@ class PaymentRequestMessage:
         graph = Graph()
         namespace = Namespace(Constants.ONTOLOGY_NAME)
         pay = namespace.__getattr__('#RequestPayment#' + str(self.name))
-        graph.add((pay, FOAF.name, Literal(self.name)))
-        graph.add((pay, FOAF.card, Literal(self.card)))
-        graph.add((pay, FOAF.ammount, Literal(self.amount)))
+        graph.add((pay, FOAF.Name, Literal(self.name)))
+        graph.add((pay, FOAF.Card, Literal(self.card)))
+        graph.add((pay, FOAF.Ammount, Literal(self.amount)))
         return graph
 
     @classmethod
     def from_graph(cls, graph):
         query = """SELECT ?x ?name ?card ?amount
             WHERE {
-                ?x ns1:name ?name.
-                ?x ns1:card ?card.
-                ?x ns1:amount ?amount.
+                ?x ns1:Name ?name.
+                ?x ns1:Card ?card.
+                ?x ns1:Amount ?amount.
             }
         """
         resp = graph.query(query)
         for f, name, card, amount in resp:
             return PaymentRequestMessage(name.toPython(), card.toPython(), amount.toPython())
-
-
-def from_graph(graph):
-    return None
--- PracticaECSDI/Messages/PaymentResponseMessage.py
+++ PracticaECSDI/Messages/PaymentResponseMessage.py
@@ -13,9 +13,9 @@ class PaymentResponseMessage:
         graph = Graph()
         namespace = Namespace(Constants.ONTOLOGY_NAME)
         prod = namespace.__getattr__('#ResponsePayment#'+str(self.name))
-        graph.add((prod, FOAF.name, Literal(self.name)))
-        graph.add((prod, FOAF.card, Literal(self.card)))
-        graph.add((prod, FOAF.amount, Literal(self.amount)))
+        graph.add((prod, FOAF.Name, Literal(self.name)))
+        graph.add((prod, FOAF.Card, Literal(self.card)))
+        graph.add((prod, FOAF.Amount, Literal(self.amount)))
         return graph
 
 
@@ -23,15 +23,11 @@ class PaymentResponseMessage:
     def from_graph(cls, graph):
         query = """SELECT ?x ?name ?card ?amount
                     WHERE {
-                        ?x ns1:name ?name.
-                        ?x ns1:card ?card.
-                        ?x ns1:amount ?amount.
+                        ?x ns1:Name ?name.
+                        ?x ns1:Card ?card.
+                        ?x ns1:Amount ?amount.
                     }
                 """
         resp = graph.query(query)
         for f, name, card, amount in resp:
             return PaymentResponseMessage(name.toPython(), card.toPython(), amount.toPython())
-
-
-def from_graph(graph):
-    return None
